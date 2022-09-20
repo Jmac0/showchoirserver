d0ae8be349daa216e4357d07888ecec04b87b9a5
JamieMac <Jamiejmackenzie@gmail.com>
2022-09-20 21:20:14 +0100

    Deployed to heroku

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.use("/", (req: Request, res: Response) => {
+app.use("/log", (req: Request, res: Response) => {
+  res.status(200).json({ message: "log" });
+});
+app.use("/hello", (req: Request, res: Response) => {
   res.status(200).json({ message: "hello World of warcraft" });
 });
 
